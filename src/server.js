7cbec9fd7e17f4b60439ef5e29a1250e6898755e
Gabriela Menezes <gabimenezess10@gmail.com>
2020-08-08 20:03:50 -0300

    add a object/teacher to the array proffy on backend structure

--- src/server.js
+++ src/server.js
@@ -24,11 +24,22 @@ const proffys = [
     {name: "Gabriela Menezes", 
     avatar: "https://avatars3.githubusercontent.com/u/64161478?s=460&u=1356f8b491eecccaeb9afb16c34beb8a00c75cf3&v=4", 
     whatsapp: "71993355025", 
-    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química",
+    bio: "Aluna de graduação em Sistemas de Informação (UNA). Entusiasta do desenvolvimento e professora nas horas vagas",
+    subject: "História",
     cost: "20", 
     weekday: [0], 
     time_from: [720], 
+    time_to: [1220]
+    },
+
+    {name: "Wendel Marques",
+    avatar: "https://avatars3.githubusercontent.com/u/6981085?s=460&u=ec7deccab13b9f8d3ca515f2374014f9127abb6a&v=4",
+    whatsapp: "62992542372",
+    bio: "Aluno de graduação em Ciência da Computação (UFG). Idealizador do Vestgeek (vestgeek.com). Data Scientist e professor nas horas vagas",
+    subject: "Matemática",
+    cost: "20",
+    weekday: [0],
+    time_from: [720],
     time_to: [1220]
     },
     
