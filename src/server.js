2d7fe201fbc9bbb40226b4b2a9bfd0cf04c9cd0b
Gabriela Menezes <gabimenezess10@gmail.com>
2020-08-09 20:38:49 -0300

    finalização do projeto

--- src/server.js
+++ src/server.js
@@ -1,113 +1,14 @@
-//Dados
 
-const proffys = [
-    {name: "Diego Fernandes", 
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-    whatsapp: "986743225", 
-    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química",
-    cost: "20", 
-    weekday: [0], 
-    time_from: [720], 
-    time_to: [1220]
-    },
-
-    {name: "Daniele Evangelista", 
-    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4", 
-    whatsapp: "986743225", 
-    bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
-    subject: "Química",
-    cost: "20", 
-    weekday: [0], 
-    time_from: [720], 
-    time_to: [1220]
-    },
-
-    {name: "Gabriela Menezes", 
-    avatar: "https://avatars3.githubusercontent.com/u/64161478?s=460&u=1356f8b491eecccaeb9afb16c34beb8a00c75cf3&v=4", 
-    whatsapp: "71993355025", 
-    bio: "Aluna de graduação em Sistemas de Informação (UNA). Entusiasta do desenvolvimento e professora nas horas vagas",
-    subject: "História",
-    cost: "20", 
-    weekday: [0], 
-    time_from: [720], 
-    time_to: [1220]
-    },
-
-    {name: "Wendel Marques",
-    avatar: "https://avatars3.githubusercontent.com/u/6981085?s=460&u=ec7deccab13b9f8d3ca515f2374014f9127abb6a&v=4",
-    whatsapp: "62992542372",
-    bio: "Aluno de graduação em Ciência da Computação (UFG). Idealizador do Vestgeek (vestgeek.com). Data Scientist e professor nas horas vagas",
-    subject: "Matemática",
-    cost: "20",
-    weekday: [0],
-    time_from: [720],
-    time_to: [1220]
-    },
-    
-]
-
-const subjects = [
-    "Artes",
-    "Biologia",
-    "Ciências",
-    "Educação física",
-    "Física",
-    "Geografia",
-    "História",
-    "Matemática",
-    "Português",
-    "Química",
-    "Inglês"
-]
-
-const weekdays = [
-    "Domingo",
-    "Segunda",
-    "Terça",
-    "Quarta",
-    "Quinta",
-    "Sexta",
-    "Sábado"
-]
-
-//funcionalidades
-
-function getSubject(subjectNumber) {
-    const arrayPosition = +subjectNumber - 1
-    return subjects[arrayPosition];
-}
-function pageLanding(req, res) {
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    const filters = req.query
-    return res.render("study.html", {proffys, filters, subjects, weekdays})
-}
-
-function pageGiveClasses(req, res) {
-    const data = req.query
-
-    // []
-    const isNotEmpty = Object.keys(data).length > 0 //pegando as chaves desse objeto e transformando em um aray
-
-    //se tiver data
-    if(isNotEmpty) {
-
-        data.subject = getSubject(data.subject) //data.subject é a matéria que foi preenchida pelo professor que quer se cadastrar! data = req.query e subject é a matéria
-        //adicionar data a lista de proffys (adicionando proffy)
-        proffys.push(data)
-        return res.redirect("/study")
-    }
-    //se nao, mostrar a pagina
-    return res.render("give-classes.html", {subjects, weekdays})
-
-}
 // pegar uma dependência que está no projeto
 const express = require('express')
 const server = express()
 
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
 //configurar nunjucks
 const nunjucks = require('nunjucks')
@@ -117,11 +18,13 @@ nunjucks.configure('src/views', {
 })
 // configurar arquivos estáticos (css, scripts, imagens)
 server.use(express.static("public"))
-
+// receber os dados do req.body
+.use(express.urlencoded({ extended: true}))
 
 // rotas da aplicação
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
+.post("/save-classes", saveClasses)
 //start do servidor
 .listen(5500)
---
+++ src/utils/format.js
@@ -0,0 +1,42 @@
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+    "Inglês"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado"
+]
+
+//funcionalidades
+
+function getSubject(subjectNumber) {
+    const arrayPosition = +subjectNumber - 1
+    return subjects[arrayPosition];
+}
+
+function convertHoursToMinutes(time) {
+    const [hour, minutes] = time.split(":")
+    return Number((hour * 60) + minutes)
+}
+
+module.exports = {
+    subjects,
+    weekdays,
+    getSubject,
+    convertHoursToMinutes
+}
